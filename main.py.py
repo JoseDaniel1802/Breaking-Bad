d61c9be2f9c8e768b428a41210fd79d35b725c00
JoseDaniel1802 <124645280+JoseDaniel1802@users.noreply.github.com>
2023-02-14 09:55:16 -0600

    FInal
    Ultima Actualizacion

--- main.py.py
+++ main.py.py
@@ -1,2 +1,12 @@
 print("Jose Daniel Cifuentes Carrascosa")
 print("1548423")
+
+import os
+
+mi ubicacion = os.getcwd()
+if os.path.exists("modulos")
+    print("La carpeta ya existe")
+else:
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
